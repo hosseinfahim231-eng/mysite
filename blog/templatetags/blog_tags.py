026e2255ac63120a394513dbf1250f4395f5defb
hossein-fahim <hosseinfahim231@gmail.com>
2026-02-18 19:42:53 +0330

    fixed

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,32 @@
+from django import template
+register = template.Library()
+from blog.models import Post
+from blog.models import category
+
+@register.simple_tag(name= 'totalposts')
+def function():
+    posts = Post.objects.filter(status= 1).count()
+    return posts
+
+@register.simple_tag(name= 'posts')
+def function():
+    posts = Post.objects.filter(status= 1)
+    return posts
+
+@register.filter
+def snippet(value,arg=20):
+    return value[:arg] + "..."
+
+@register.inclusion_tag('blog/blog-popular-posts.html')
+def latestposts(arg=3):
+    posts = Post.objects.filter(status= 1).order_by('published_date')[:arg]
+    return {'posts':posts}
+
+@register.inclusion_tag('blog/blog-post-categories.html')
+def post_categories():
+    posts = Post.objects.filter(status= 1)
+    categories = category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name] = posts.filter(category=name).count()
+    return { 'categories': cat_dict}
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,45 @@
 from django.shortcuts import render,get_object_or_404
 from blog.models import Post
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
-def blog_view(request):
+def blog_view(request,cat_name=None,author_username=None):
      posts = Post.objects.filter(status=1)
+     if cat_name:
+         posts = posts.filter(category__name=cat_name)
+     if author_username:
+         posts = posts.filter(author__username=author_username)
+
+     posts = Paginator(posts, 3)
+     try:
+         page_number = request.GET.get('page')
+         posts = posts.get_page(page_number)
+     except PageNotAnInteger:
+         posts = posts.get_page(1)
+     except EmptyPage:
+         posts = posts.get_page(1)
      context = {'posts': posts}
      return render(request,'blog/blog-home.html',context)
+
 
 def blog_single(request,pid):
     post = get_object_or_404(Post, pk=pid,status=1)
     context = {'post': post}
     return render(request,'blog/blog-single.html',context)
 
-def test(request,pid):
-    post = get_object_or_404(Post,pk=pid)
-    context = {'post':post}
-    return render(request,'test.html',context)
+def test(request):
+    return render(request,'test.html')
+
+def blog_category(request,cat_name):
+    posts = Post.objects.filter(status=1)
+    posts = posts.filter(category__name=cat_name)
+    context = {'posts': posts}
+    return render(request,'blog/blog-home.html',context)
+
+def blog_search(request):
+    posts = Post.objects.filter(status=1)
+    if request.method == "GET":
+        if s := request.GET.get('s'):
+            posts = posts.filter(content__contains=s)
+
+    context = {'posts': posts}
+    return render(request,'blog/blog-home.html',context)
